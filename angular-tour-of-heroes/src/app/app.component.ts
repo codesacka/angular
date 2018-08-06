064a30f09a811bb9d3154e3bae723285a9bd3d44
klunwebale <klunwebale@gmail.com>
2018-08-06 21:22:11 +0300

    changes

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Heroes';
+  title = 'Angular Heroes title';
+  title2 = 'Angular Heroes title Here';
+
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
+
   
 })
 export class HeroesComponent implements OnInit {
@@ -15,6 +17,12 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
